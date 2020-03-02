dbf978ef6d5774760e99bfe9cbde8a5e0db7e3e0
Sam Levy <slevy239@gmail.com>
2020-03-02 11:53:06 -0500

    inventory update

--- vue.js
+++ vue.js
@@ -1,6 +1,13 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/81PTFhxgEwL._AC_UL1500_.jpg',
+        Instock: true,
+        inventory: 8,
+
+
+
+        
     }
 })
