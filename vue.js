12654c561deaeacb7ec85c3706fd7f4f6465aeb3
Sam Levy <slevy239@gmail.com>
2020-03-02 12:24:57 -0500

    mouseOver function

--- vue.js
+++ vue.js
@@ -2,25 +2,32 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: 'https://images-na.ssl-images-amazon.com/images/I/81PTFhxgEwL._AC_UL1500_.jpg',
         // Instock: true,
         inventory: 0,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/81PTFhxgEwL._AC_UL1500_.jpg',
         details: ['80% cotton', '20% polyester', 'Uni-sex'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: "black"
+                variantColor: "black",
+                variantImage: 'https://images-na.ssl-images-amazon.com/images/I/81PTFhxgEwL._AC_UL1500_.jpg',
             },
             {
                 variantId: 2235,
-                variantColor: 'white'
+                variantColor: 'white',
+                variantImage: 'https://dks.scene7.com/is/image/GolfGalaxy/SX4445_101-White_FRT?wid=685&fmt=jpg'
             }
         ],
         cart: 0
     },
     methods: {
-        addToCart: function () {
+        addToCart() {
             this.cart += 1 
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+
         }
     }
+
 })
