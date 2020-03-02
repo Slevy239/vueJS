d314ba43ec056c0ff357d38fa171f63e95f461b6
Sam Levy <slevy239@gmail.com>
2020-03-02 18:43:18 -0500

    dynamic validation

--- vue.js
+++ vue.js
@@ -116,6 +116,16 @@ Vue.component('product', {
 Vue.component('product-review', {
     template: `
 <form class="review-form" @submit.prevent="onSubmit">
+
+<p v-if="errors.length">
+    <b>Please Correct the following error(s):</b>
+    <ul>
+    <li v-for="error in errors">{{ error }}</li>
+    </ul>
+</p>
+
+
+
 <p>
 <label for="name">Name:</label>
 <input id="name" v-model="name">
@@ -123,7 +133,7 @@ Vue.component('product-review', {
 
 <p>
 <label for="review">Review:</label>
-<textarea id="review" v-model="review" required></textarea>
+<textarea id="review" v-model="review"></textarea>
 </p>
 
 <p>
@@ -147,20 +157,29 @@ Vue.component('product-review', {
         return {
             name: null,
             review: null,
-            rating: null
+            rating: null,
+            errors: []
         }
     },
     methods: {
         onSubmit() {
-            let productReview = {
-                name: this.name,
-                review: this.review,
-                rating: this.rating
+            if (this.name && this.review && this.rating) {
+
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
             }
-            this.$emit('review-submitted', productReview)
-            this.name = null
-            this.review = null
-            this.rating = null
+            else {
+                if (!this.name) this.errors.push("Name Required")
+                if (!this.review) this.errors.push("review Required")
+                if (!this.rating) this.errors.push("rating Required")
+            }
         }
     }
 })
