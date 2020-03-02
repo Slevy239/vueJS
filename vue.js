7fbea0ddcd0cdecf5a1b1b89ad8de05c0ac517a1
Sam Levy <slevy239@gmail.com>
2020-03-02 12:13:38 -0500

    click functions

--- vue.js
+++ vue.js
@@ -3,11 +3,24 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: 'https://images-na.ssl-images-amazon.com/images/I/81PTFhxgEwL._AC_UL1500_.jpg',
-        Instock: true,
-        inventory: 8,
-
-
-
-        
+        // Instock: true,
+        inventory: 0,
+        details: ['80% cotton', '20% polyester', 'Uni-sex'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "black"
+            },
+            {
+                variantId: 2235,
+                variantColor: 'white'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1 
+        }
     }
 })
