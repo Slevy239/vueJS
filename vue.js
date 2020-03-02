0fa116f365cdaa4995299bbad06db2689cf69de6
Sam Levy <slevy239@gmail.com>
2020-03-02 18:28:49 -0500

    reviews

--- vue.js
+++ vue.js
@@ -1,40 +1,82 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        brand: 'Levy',
-        product: 'Socks',
-        // Instock: true,
-        // inventory: 20,
-        // inStock: true,
-        selectedVariant: 0,
-        // image: 'https://images-na.ssl-images-amazon.com/images/I/81PTFhxgEwL._AC_UL1500_.jpg',
-        details: ['80% cotton', '20% polyester', 'Uni-sex'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "black",
-                variantImage: 'https://images-na.ssl-images-amazon.com/images/I/81PTFhxgEwL._AC_UL1500_.jpg',
-                variantQty: 15
-            },
-            {
-                variantId: 2235,
-                variantColor: 'red',
-                variantImage: 'https://dks.scene7.com/is/image/GolfGalaxy/SX4445_101-White_FRT?wid=685&fmt=jpg',
-                variantQty: 0,
-            }
-        ],
-        cart: 0
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `<div id="app">
+    <div class="product">
+        <div class="product-image">
+            <img v-bind:src="image">
+        </div>
+
+        <div class="product-info">
+
+            <h1>{{ title }}</h1>
+            <p v-if='inStock'>In Stock</p>
+            <p v-if='variants[selectedVariant].variantQty > 0'>{{ variants[selectedVariant].variantQty }} left in stock</p>
+            <p v-else>Out of Stock!</p>
+            <p>Shipping: {{ shipping }}</p >
+
+            <ul>
+                <li v-for='detail in details'>{{ detail }}</li>
+            </ul>
+            <div v-for='(variant, index) in variants' 
+            :key='variant.variantId' 
+            class="color-box"
+            :style='{ backgroundColor: variant.variantColor }'
+            @mouseover='updateProduct(index)'>
+            </div>
+
+
+            <button v-on:click='addToCart' 
+            :disabled='!inStock'
+            :class='{disabledButton: !inStock}'
+            >Add to Cart</button>
+
+        </div>
+<product-review @review-submitted="addReview"></product-review>
+    </div>
+</div>`,
+    data() {
+        return {
+            brand: "Levy's",
+            product: 'Socks',
+            // Instock: true,
+            // inventory: 20,
+            // inStock: true,
+            selectedVariant: 0,
+            // image: 'https://images-na.ssl-images-amazon.com/images/I/81PTFhxgEwL._AC_UL1500_.jpg',
+            details: ['80% cotton', '20% polyester', 'Uni-sex'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "black",
+                    variantImage: 'https://images-na.ssl-images-amazon.com/images/I/81PTFhxgEwL._AC_UL1500_.jpg',
+                    variantQty: 15
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'red',
+                    variantImage: 'https://m.media-amazon.com/images/I/71KQw2f9taL._SR500,500_.jpg',
+                    variantQty: 12,
+                }
+            ],
+            reviews: []
+        }
     },
     methods: {
-        addToCart() {
-            this.cart += 1 
-        },
-        removeCart() {
-            this.cart -= 1
-        },
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index)
+        },
+        addToCart() {
+            this.$emit('add-to-cart',
+                this.variants[this.selectedVariant].variantId)
+        },
+        addReview(productReview) {
+             this.reviews.push(productReview)
         }
     },
     computed: {
@@ -46,7 +88,81 @@ var app = new Vue({
         },
         inStock() {
             return this.variants[this.selectedVariant].variantQty
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free"
+            } else {
+                return "$2.99"
+            }
         }
+    }
+})
+
+Vue.component('product-review', {
+    template: `
+<form class="review-form" @submit.prevent="onSubmit">
+<p>
+<label for="name">Name:</label>
+<input id="name" v-model="name">
+</p>
+
+<p>
+<label for="review">Review:</label>
+<textarea id="review" v-model="review"></textarea>
+</p>
+
+<p>
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+        <option>5</option>
+        <option>4</option>
+        <option>3</option>
+        <option>2</option>
+        <option>1</option>
+    </select>
+    </p>
+
+    <p>
+    <input type="submit" value="Submit">
+    </p>
+
+    </form>
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null
+        }
+    },
+    methods: {
+        onSubmit() {
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating
+            }
+            this.$emit('review-submitted', productReview)
+            this.name = null
+            this.review = null
+            this.rating = null
+        }
+    }
+})
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        premium: true,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+
     }
 
 })
